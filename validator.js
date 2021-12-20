71703c9d0b87b087d054a57042fe100c81f7aaec
Waleed Ashraf <waleedashraf@outlook.com>
2021-12-21 00:35:44 +0100

    bump version

--- validator.js
+++ validator.js
@@ -8,6 +8,7 @@ const validate = async (filePath) => {
 
   const dir = process.env.GITHUB_WORKSPACE || __dirname
   const fullPath = path.resolve(dir, filePath)
+  console.log(`schema filePath:${filePath}`)
   console.log(`schema file full path:${fullPath}`)
   const data = fs.readFileSync(fullPath, 'utf8')
   await parser.parse(data, {path: filePath})
